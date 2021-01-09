a1599a618f352cb1b257b72a401c5b91b0a7828c
dawti <dawtiej@gmail.com>
2021-01-09 10:08:51 +0530

    O02Backend

--- com.springboot/src/main/java/com/springboot/controller/TestController.java
+++ com.springboot/src/main/java/com/springboot/controller/TestController.java
@@ -1,16 +1,19 @@
 package com.springboot.controller;
 
-
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 	
 	@CrossOrigin(origins = "*")
 	@RestController
 	@RequestMapping("/api/shopping")
 	public class TestController {
+		
+			
 		@GetMapping("/all")
 		@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
 		public String allAccess() {
