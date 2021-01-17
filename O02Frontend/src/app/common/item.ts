8161523c37e6ee6a282c800e9bfc82ff36e66c85
Soham Sarkar <74293268+sohamsarkar17@users.noreply.github.com>
2021-01-17 19:46:35 +0530

    Add files via upload

---
+++ O02Frontend/src/app/common/item.ts
@@ -0,0 +1,12 @@
+export class Item {
+
+    itemid: number;
+    itemname: string;
+    description: string;
+    itemprice: number;
+    stock: number;
+    imageurl: string;
+    itemcategoryid: number; 
+
+
+}
